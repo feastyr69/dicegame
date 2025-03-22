1ab5f2c24a0c6940379b63d5e1067295a57fde53
feastyr69 <164464915+feastyr69@users.noreply.github.com>
2025-03-22 21:48:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const diceValues = document.querySelectorAll(".num");
 const btn = document.querySelector("button");
 const out = document.querySelector(".rolls");
 const res = document.querySelector(".result");
+const end = document.querySelector(".numbers");
 let rolls = 0;
 let logs = 0;
 let done = [0, 0, 0, 0, 0, 0];
@@ -37,6 +38,7 @@ btn.addEventListener("click", () => {
         else{
             comment="NAH THAT'S IMPOSSIBLE DUDE WTF <br>I'M JEALOUS"
         }
+        end.classList.add("active");
         out.innerHTML = "Congrats you won!</p><br>"+ "<br>" + comment + "</p><p> Refresh the page to Play Again.";
         res.textContent = Math.floor(100 * 6 / rolls) + "/100";
         document.body.style.filter = 'invert(1)';
